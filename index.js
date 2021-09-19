dcf61d64e19e77adc7e933ebc7ce119a84754a60
Luke Hovarter <lukehsurvey@gmail.com>
2021-09-19 17:21:03 -0400

    changed the poweronserver command to give info

--- index.js
+++ index.js
@@ -17,12 +17,25 @@ client.on('interactionCreate', async interation => {
 
     switch(commandName) {
         case 'poweronserver':
+            await interation.deferReply("Turning on server");
             out = await exec(`wakeonlan ${ MAC }`);
-            await interation.reply(!out.stderr ? "Starting Server!" : "Error");
+            await interation.editReply("Command sent...");
+            out = await exec(`ping ${ IP } -c 1 | grep received | awk ' { print $4 } '`);
+
+            let count = 0;
+            while (+out.stdout === 0 && count < 20) {
+                out = await exec(`ping ${ IP } -c 1 | grep received | awk ' { print $4 } '`);
+                await interation.editReply(`Server starting up ${ count }`);
+                count++;
+                await new Promise(resolve => setTimeout(resolve, 3000));
+            }
+
+            await interation.editReply(+out.stdout > 0 ? 'Server started!' : 'Timeout error');
         break;
         case 'checkstatus':
+            await interation.deferReply("Checking Server");
             out = await exec(`ping ${ IP } -c 1 | grep received | awk ' { print $4 } '`);
-            await interation.reply(+out.stdout > 0 ? 'Server On' : 'Server Off');
+            await interation.editReply(+out.stdout > 0 ? 'Server On' : 'Server Off');
         break;
         case 'startvalheim':
             await interation.deferReply("Working");
